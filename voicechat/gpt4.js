5b65bea0d0635bb43ffb3daf530280712241de11
ubrainjp <116853051+ubrainjp@users.noreply.github.com>
2023-04-10 11:47:48 +0900

    Add files via upload

--- voicechat/gpt4.js
+++ voicechat/gpt4.js
@@ -254,7 +254,7 @@ window.addEventListener('load', function(){
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/voicechat/serviceWorker.js',
-       { scope: '/voicechat/' })
+       { scope: '/ubrainjp.github.io/voicechat/' })
   .then(function(reg) {
     console.log('serviceWorker:' + reg.scope);
   }).catch(function(error) {
