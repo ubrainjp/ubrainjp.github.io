5f305f6ed332e4e2988d42d93b5829016cbd4f93
ubrainjp <116853051+ubrainjp@users.noreply.github.com>
2023-04-10 11:52:53 +0900

    Add files via upload

--- voicechat/serviceWorker.js
+++ voicechat/serviceWorker.js
@@ -1,3 +1,9 @@
+self.addEventListener('install', (event) => {
+  console.log('service worker install ...');
+});
+self.addEventListener('activate', (event) => {
+console.info('activate', event);
+});
 self.addEventListener('fetch', function(event) {
 });
 
